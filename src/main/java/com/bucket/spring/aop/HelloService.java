ea4ca415f31119fe1912cefc5afa8d3ca810a8f1
chengcanmm77 <937236042@qq.com>
2017-02-05 21:04:20 +0800

    www

---
+++ src/main/java/com/bucket/spring/aop/HelloService.java
@@ -0,0 +1,8 @@
+package com.bucket.spring.aop;
+
+public class HelloService {
+
+	public void sayHello() {
+		System.out.println("sayHello.....");
+	}
+}
---
+++ src/main/java/com/bucket/spring/aop/IocTest.java
@@ -0,0 +1,14 @@
+package com.bucket.spring.aop;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class IocTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("application.xml");
+		HelloService helloService = ac.getBean("helloService", HelloService.class);
+		helloService.sayHello();
+		System.out.println(ac.getParentBeanFactory().getClass().getName());
+	}
+}
