ef9159dbc1c4356e9f4fc497538849f032df047e
chengcanmm77 <937236042@qq.com>
2017-02-06 20:15:13 +0800

    dd

--- src/main/java/com/bucket/spring/aop/IocTest.java
+++ src/main/java/com/bucket/spring/aop/IocTest.java
@@ -1,6 +1,7 @@
 package com.bucket.spring.aop;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IocTest {
@@ -9,6 +10,7 @@ public class IocTest {
 		ApplicationContext ac = new ClassPathXmlApplicationContext("application.xml");
 		HelloService helloService = ac.getBean("helloService", HelloService.class);
 		helloService.sayHello();
-		System.out.println(ac.getParentBeanFactory().getClass().getName());
+		
+		System.out.println(((MessageSource)ac).getMessage("message", null, "Default", null));
 	}
 }
